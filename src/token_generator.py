e757d2aa3c3f94b4826eb89c57fc5d0fea97139e
Eric Arellano <ericarellano@me.com>
2019-07-15 10:33:55 -0700

    Link to docs for the scope

--- src/token_generator.py
+++ src/token_generator.py
@@ -10,9 +10,13 @@ resource_name = credentials_client.service_account_path(
     project="-", service_account="travis-ci-rbe@pants-remoting-beta.iam.gserviceaccount.com"
 )
 
+# NB: This may either be `auth/cloud-platform` or `auth/iam`, per
+# https://cloud.google.com/iam/docs/creating-short-lived-service-account-credentials#sa-credentials-oauth
+scope = ["https://www.googleapis.com/auth/cloud-platform"]
+
 
 def generate() -> str:
     access_token: str = credentials_client.generate_access_token(
-        name=resource_name, scope=["https://www.googleapis.com/auth/cloud-platform"]
+        name=resource_name, scope=scope
     ).access_token
     return access_token
