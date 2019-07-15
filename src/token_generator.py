08d389ca8fc9a540b31340fd378644f60239a760
Eric Arellano <ericarellano@me.com>
2019-07-15 10:18:57 -0700

    Fix issue with bad scope argument

--- src/token_generator.py
+++ src/token_generator.py
@@ -2,16 +2,6 @@ from __future__ import annotations
 
 from google.cloud import iam_credentials_v1
 
-RBE_EXECUTE_TEST_SCOPES = [
-    "https://www.googleapis.com/auth/remotebuildexecution.actions.create",
-    "https://www.googleapis.com/auth/remotebuildexecution.actions.get",
-    "https://www.googleapis.com/auth/remotebuildexecution.blobs.create",
-    "https://www.googleapis.com/auth/remotebuildexecution.blobs.get",
-    "https://www.googleapis.com/auth/remotebuildexecution.logstreams.create",
-    "https://www.googleapis.com/auth/remotebuildexecution.logstreams.get",
-    "https://www.googleapis.com/auth/remotebuildexecution.logstreams.update",
-]
-
 credentials_client = iam_credentials_v1.IAMCredentialsClient()
 
 # NB: The project name must be a wildcard `-`, per
@@ -23,6 +13,6 @@ resource_name = credentials_client.service_account_path(
 
 def generate() -> str:
     access_token: str = credentials_client.generate_access_token(
-        name=resource_name, scope=RBE_EXECUTE_TEST_SCOPES
+        name=resource_name, scope=["https://www.googleapis.com/auth/cloud-platform"]
     ).access_token
     return access_token
